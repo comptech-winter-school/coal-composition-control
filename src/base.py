ea9c37742ab05a726de66df7ecf8075b4b8516b6
Alexander Lekomtsev <69035428+JI411@users.noreply.github.com>
2022-01-31 20:07:33 +0700

    get fraction return coal size

--- src/base.py
+++ src/base.py
@@ -21,16 +21,8 @@ class Coals:
 
     def __init__(self, rectangles):
         self.rectangles = rectangles
-        self.size_map = self.get_size_map()
 
-    @staticmethod
-    def get_size_map() -> Dict[str, Tuple[int, int]]:
-        # with open(path) as f:
-        #     size_map = json.load(f)
-        size_map = {'s': (100, 199), 'm': (200, 299), 'l': (299, np.inf)}
-        return size_map
-
-    def get_fraction(self):
+    def get_fraction(self) -> NDArray:
         raise NotImplementedError()
 
 
@@ -44,12 +36,7 @@ class InstanceSegmentationCoals(Coals):
     def longest_side(box):
         return max(np.linalg.norm(box[0] - box[1]), np.linalg.norm(box[1] - box[2]))
 
-    def get_fraction(self):
+    def get_fraction(self) -> NDArray:
         boxes = [np.int0(cv2.boxPoints(rect)) for rect in self.rectangles]
-        diagonal = np.array([self.longest_side(box) for box in boxes])
-        fractions = {
-            cat: np.count_nonzero((size[0] < diagonal) & (diagonal < size[1]))
-            for cat, size in self.size_map.items()
-        }
-        return fractions
+        return np.array([self.longest_side(box) for box in boxes])
 
