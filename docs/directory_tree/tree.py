60c1c0b94562b2538bbaaecbbbf74eb39203e7ec
LRDPRDX <paradox1859@gmail.com>
2022-02-02 01:39:32 +0700

    Add program for automatic dir tree creation

--- docs/directory_tree/tree.py
+++ docs/directory_tree/tree.py
@@ -5,27 +5,34 @@ class TikzTreeProducer:
 
     def __init__(self):
         self.patterns = []
+        self.include_hidden = False
+        self.style_name = "horizontal"
         self.escapes = {}
 
     def get_tree(self, root_dir):
+        print(self.tree_header())
         tree = os.walk(root_dir)
         level_not_closed = -1
-        for (root, _, files) in tree :
+        for (root, dirs, files) in tree :
+            root = root[len(root_dir):]
+            if( not self.include_hidden ) :
+                dirs[:] = [d for d in dirs if not d[0] == '.']
             current_level = root.count( os.sep )
-            if( current_level <= level_not_closed ) :
+            if( current_level <= level_not_closed ):
                 self.close_nodes( level_not_closed, current_level )
                 level_not_closed = current_level
             else :
                 level_not_closed += 1
-            self.indent_node( current_level, self.open_node( self.escape( os.path.basename( root ) ) ) )
+            self.indent_node( current_level, self.open_dir( self.escape( os.path.basename( root ) ) ) )
             f_match = lambda f_name : any(fnmatch.fnmatch( f_name, p ) for p in self.patterns)
             for f in filter(f_match, files):
-                self.indent_node( current_level + 1, self.open_close_node( self.escape( f ) ) )
+                self.indent_node( current_level + 1, self.open_file( self.escape( f ) ) )
         else :
             self.close_nodes( level_not_closed, 0 )
+        print(self.tree_trailer())
 
-    def open_node(self, name):
-        return '[' + name
+    def open_dir(self, name):
+        return f'[{name}, dir'
 
     def close_nodes(self, fromN, toN) :
         for i in range(fromN, toN - 1, -1):
@@ -39,13 +46,19 @@ class TikzTreeProducer:
     def indent_node(self, level, name):
         print( 4 * level * ' ' + name )
 
-    def open_close_node(self, name) :
-        return '[' + name + ']'
+    def open_file(self, name) :
+        return f'[{name}, file]'
+
+    def tree_header(self):
+        return r'\begin{forest} for tree={' + self.style_name + '}'
+
+    def tree_trailer(self):
+        return r'\end{forest}'
 
 
 if(__name__ == "__main__") :
     t = TikzTreeProducer()
-    t.patterns = ["*.py", "*.md"]
+    t.patterns = ["*.py", "*.md",]
     t.escapes = {'_' : r'$\textunderscore$'}
 
-    t.get_tree("..")
+    t.get_tree("/home/lrdprdx/projects/winter_school/coal-composition-control")
