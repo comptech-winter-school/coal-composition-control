a6db2c8ae543558fdaa6f67018370e28ae2477e9
Alexander Lekomtsev <69035428+JI411@users.noreply.github.com>
2022-02-02 10:53:00 +0700

    fix bugs with path and rgb convert
    Took 34 minutes

--- src/instance_segmentation/mask_rcnn.py
+++ src/instance_segmentation/mask_rcnn.py
@@ -39,7 +39,8 @@ class MaskRCNN(BasePredictor):
 
 
 if __name__ == '__main__':
-    image = cv2.imread(str(DATA_DIR / 'few_data_split' / 'train' / '20210712_141048_857A_ACCC8EAF31F3_0.jpg'))
+    image = cv2.imread(str(DATA_DIR / 'few_data_split' / 'few_data_train' / '20210712_141048_857A_ACCC8EAF31F3_0.jpg'))
+    image = image[..., ::-1]
     mask_rcnn = MaskRCNN(WEIGHTS_DIR / 'mask_rcnn.pth')
 
     coals = mask_rcnn.predict(image)
