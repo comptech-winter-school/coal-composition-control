b2300af61c852a2da14fd84b2c472cccf9d50e82
Alexander Lekomtsev <69035428+JI411@users.noreply.github.com>
2022-02-03 14:39:56 +0700

    add example_app.py

---
+++ example_app.py
@@ -0,0 +1,23 @@
+import cv2
+
+from constants import DATA_DIR, WEIGHTS_DIR
+from scripts.download import get_data, get_weights
+from src.instance_segmentation.edge_segmentation import EdgeSegmentation
+from src.instance_segmentation.mask_rcnn import MaskRCNN
+
+if __name__ == '__main__':
+    get_data()
+    get_weights()
+
+    image = cv2.imread(str(DATA_DIR / 'few_data_split' / 'few_data_train' / '20210712_141048_857A_ACCC8EAF31F3_0.jpg'))
+
+    if image is None:
+        raise FileNotFoundError('check path to image')
+
+    mask_rcnn = MaskRCNN(WEIGHTS_DIR / 'mask_rcnn.pth')
+    coals = mask_rcnn.predict(image)
+    print([coal.get_fraction() for coal in coals])
+
+    edge_segmentation = EdgeSegmentation(WEIGHTS_DIR / 'edge_segmentation.pth')
+    coals = edge_segmentation.predict(image)
+    print([coal.get_fraction() for coal in coals])
--- src/instance_segmentation/mask_rcnn.py
+++ src/instance_segmentation/mask_rcnn.py
@@ -38,12 +38,13 @@ class MaskRCNN(BasePredictor):
 
 
 if __name__ == '__main__':
-    image = cv2.imread(str(DATA_DIR / 'few_data_split' / 'train' / '20210712_141048_857A_ACCC8EAF31F3_0.jpg'))
+    image = cv2.imread(str(DATA_DIR / 'few_data_split' / 'few_data_train' / '20210712_141048_857A_ACCC8EAF31F3_0.jpg'))
     mask_rcnn = MaskRCNN(WEIGHTS_DIR / 'mask_rcnn.pth')
 
     coals = mask_rcnn.predict(image)
     print([coal.get_fraction() for coal in coals])
 
-    cv2.imshow('Contours', coals[0].plot_on(image))
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    if coals:
+        cv2.imshow('Contours', coals[0].plot_on(image))
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
