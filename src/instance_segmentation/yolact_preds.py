5a4058664141cbf2e17c364afc601c9e13dd3d19
Dr_Under <send2mybox@mail.ru>
2022-02-04 17:19:07 +0500

    adding yolact class code

--- src/instance_segmentation/yolact_preds.py
+++ src/instance_segmentation/yolact_preds.py
@@ -1,5 +1,5 @@
 import sys
-sys.path.insert(0, '/content/yolact')
+# sys.path.insert(0, '../yolact')
 import eval
 import layers
 from data import COCODetection, get_label_map, MEANS, COLORS
@@ -13,8 +13,6 @@ import numpy as np
 from PIL import Image
 from pathlib import Path
 from types import SimpleNamespace
-# from dataset import CoalFractionDataset
-from src.base import BasePredictor, InstanceSegmentationCoal
 
 from constants import EXAMPLE_IMG, WEIGHTS_DIR
 from src.base import BasePredictor, InstanceSegmentationCoal
@@ -29,7 +27,7 @@ class YolactPredictor(BasePredictor):
     def get_model(weights_path, args):
         model = Yolact()
         map_location = None if args.cuda else torch.device('cpu')
-        model.load_weights(args.trained_model, map_location=map_location)
+        model.load_weights(weights_path, map_location=map_location)
         return model
 
     @torch.no_grad()
@@ -39,11 +37,10 @@ class YolactPredictor(BasePredictor):
                         crop_masks=self.args.crop,
                         score_threshold=self.args.score_threshold)
         idx = t[1].argsort(0, descending=True)[:self.args.top_k]
-
         if args.eval_mask_branch:
             masks = t[3][idx]
         classes, scores, boxes = [x[idx].cpu().detach().numpy() for x in t[:3]]
-        return InstanceSegmentationStone(masks=np.array(masks))
+        return InstanceSegmentationCoal(masks=np.array(masks))
 
 if __name__ == '__main__':
     args_dict = {'images': '/content/few_data:/content/output_images',
@@ -52,24 +49,17 @@ if __name__ == '__main__':
              'mask_proto_debug': False,
              'cross_class_nms': False,
              'video': None,
-             'image': '/content/few_data/20210712_141048_857A_ACCC8EAF31F3_0.jpg',
-             'trained_model': '/content/yolact_base_201_2013_interrupt.pth',
              'crop': False,
              'score_threshold': 0.05,
              'display_lincomb': False,
              'top_k': 15,
              'eval_mask_branch': True,
              }
-    global args
     args = SimpleNamespace(**args_dict)
-    eval.set_args(args)
-    eval.get_args()
-
-    image = str(EXAMPLE_IMG)
-    yol_model = YolactPredictor(WEIGHTS_DIR / '....pth', args)
+    image = cv2.imread(str(EXAMPLE_IMG))
+    yol_model = YolactPredictor(WEIGHTS_DIR / 'yolact_base_201_2013_interrupt.pth', args)
     coals = yol_model.predict(image)
     print([coal.get_fraction() for coal in coals])
-
     cv2.imshow('Contours', coals[0].plot_on(image))
     cv2.waitKey(0)
     cv2.destroyAllWindows()
