fcb40e4376005b141f3524d39825753c02c3a000
Alexander Lekomtsev <69035428+JI411@users.noreply.github.com>
2022-02-05 20:39:55 +0700

    change default yoloact model
    Took 21 seconds

--- scripts/download.py
+++ scripts/download.py
@@ -57,9 +57,8 @@ def load_weights() -> None:
         md5='0232b6b3946dd5a4d547e95b09f2b76e'
     )
     gdown.cached_download(
-        id='1OyxcsG4A7BFKAL_m2GC9uk1B_qQWlE8O',
+        id='17L4Qrbyp9FAr2zsUuRgkX9SoKTa1FC5L',
         path=str(WEIGHTS_DIR / 'yolact.pt'),
-        md5='4484755dc477757665d30ba5c592a0a3'
     )
     unzip(archives_dir=WEIGHTS_DIR)
 
